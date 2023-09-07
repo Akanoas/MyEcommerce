b92c3b59ecaaf191c699137879a2624b6deae1e9
Abdoul Akano <akano1nassirou@gmail.com>
2023-09-07 17:39:56 -0400

    nav stylling

--- ecommerce/src/Nav.jsx
+++ ecommerce/src/Nav.jsx
@@ -49,21 +49,24 @@ const Nav = () => {
     </div>
     <div className="header">
         <div className="container">
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/product'>Product</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-                <li>
-                    <Link to='/contact'>Contact</Link>
-                </li>
-                
-            </ul>
+            <div className="nav">
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/product'>Product</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                    
+                </ul>
+            </div>
+           
             <div className="auth">
                 <CiLogin />
                 <BiLogOut />

f62afae4108b718df69c0f988eb92da2efcd1f8f
Abdoul Akano <akano1nassirou@gmail.com>
2023-09-23 08:01:54 -0400

    home done at 75%

--- ecommerce/src/Pages/Home.jsx
+++ ecommerce/src/Pages/Home.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom"
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import { FiTruck } from 'react-icons/fi'
+import { BsCurrencyDollar} from 'react-icons/bs'
+import { CiPercent} from 'react-icons/ci'
+import { BsHeadphones } from 'react-icons/bs'
 import './Home.css'
 export default function Home(){
     return(
@@ -17,36 +21,79 @@ export default function Home(){
             <div className="product-type">
                 <div className="container">
                     <div className="box">
-                        <div className="img-box">
-                            <img src="../../public/image/men-clothings.jpg" alt="men" />
+                        <div className="img-box-product">
+                            <img src="../../public/image/men-clothings.jpg" alt="men" className="box-img"/>
                         </div>
                         <div className="detail">
-                            <p>23products</p>
+                            <p>50 products</p>
                         </div>
                     </div>
                     <div className="box">
-                        <div className="img-box">
-                            <img src="../../public/image/women-Clothing.jpg" alt="women" />
+                        <div className="img-box-product">
+                            <img src="../../public/image/women-Clothing.jpg" alt="women"  className="box-img"/>
                         </div>
                         <div className="detail">
-                            <p>83products</p>
+                            <p>200 products</p>
                         </div>
                     </div>
                     <div className="box">
-                        <div className="img-box">
-                            <img src="../../public/image/jwerly.jpg" alt="jwerly" />
+                        <div className="img-box-product">
+                            <img src="../../public/image/jwerly.jpg" alt="jwerly" className="box-img"/>
                         </div>
                         <div className="detail">
-                            <p>23products</p>
+                            <p>89 products</p>
                         </div>
                     </div>
                     <div className="box">
-                        <div className="img-box">
-                            <img src="../../public/image/electronics.jpg" alt="electro" />
+                        <div className="img-box-product">
+                            <img src="../../public/image/electronics.jpg" alt="electro" className="box-img"/>
                         </div>
                         <div className="detail">
-                            <p>23products</p>
+                            <p>23 products</p>
                         </div>
+                    </div>
+
+                </div>
+                <div className="about">
+                    <div className="container">
+                        <div className="icon">
+                            <FiTruck />
+                        </div>
+                        <div className="detail">
+                            <h3>Free Shipping</h3>
+                            <p>Order up to $50</p>
+                        </div>
+
+                    </div>
+                    <div className="container">
+                        <div className="icon">
+                            <BsCurrencyDollar />
+                        </div>
+                        <div className="detail">
+                            <h3>Return & Refund</h3>
+                            <p>Money Back Guaranty</p>
+                        </div>
+
+                    </div>
+                    <div className="container">
+                        <div className="icon">
+                            <CiPercent />
+                        </div>
+                        <div className="detail">
+                            <h3>Member Discount</h3>
+                            <p>On every Order</p>
+                        </div>
+
+                    </div>
+                    <div className="container">
+                        <div className="icon">
+                            <BsHeadphones />
+                        </div>
+                        <div className="detail">
+                            <h3>Customer Support</h3>
+                            <p>24/7 Call</p>
+                        </div>
+
                     </div>
 
                 </div>
